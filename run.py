098dcf9b0aab2aa8f32f0f7d6c44b9b1845141ba
sangedianhao <116492672+sangedianhao@users.noreply.github.com>
2023-09-21 15:54:13 +0800

    Update run.py

--- run.py
+++ run.py
@@ -2,6 +2,8 @@ from find_feature import process_image
 from find_point import bfs
 from config import parse_arguments
 from edgeDetection import edge_detection
+from find_junction import find_nearest_points
+from json_to_xml import convert_json_to_xml
 
 # 获取超参数
 parser = parse_arguments()
@@ -11,3 +13,7 @@ image_file = process_image(parser.input_image, down_sampling=parser.down_samplin
 bfs(image_file)
 # 边缘检测
 edge_detection(parser.input_image)
+# 获取路口连接信息
+nearest_points = find_nearest_points(points, threshold=parser.M)
+# 保存xml文件
+convert_json_to_xml(nearest_points, parser.output_path, down_sampling=parser.down_sampling)
